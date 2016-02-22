b832e9283a3ca228c870740e76d0e5781fecc12d
Andrew Bonnington <andrewbonnington@users.noreply.github.com>
2016-02-22 21:14:53 +0000

    Allow passing directory argument

--- list-git-repos.sh
+++ list-git-repos.sh
@@ -1,8 +1,27 @@
 #!/bin/sh
 
-regex="^\.\/(.*)\/.git$"
+if [ $# -eq 0 ]; then
+	directory="."
+else
+	case $1 in
+		-h|--help)
+			echo "Usage: list-git-repos <directory>"
+			exit
+			;;
+	    *)
+	    	if [ -d "$1" ]; then
+				directory=$1
+			else
+				echo "$1 is not a directory"
+				exit
+			fi
+	    	;;
+	esac
+fi
 
-find . -name .git -type d -prune -maxdepth 2 -print0 | while IFS= read -r -d $'\0' line; do
+regex="^${directory}\/(.*)\/.git$"
+
+find $directory -name .git -type d -prune -maxdepth 2 -print0 | while IFS= read -r -d $'\0' line; do
     [[ $line =~ $regex ]]
     echo "${BASH_REMATCH[1]}"
 done
