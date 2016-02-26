c351b2d0a904d23dfb63af6a24f6354ecb8c828a
Andrew Bonnington <andrewbonnington@users.noreply.github.com>
2016-02-26 19:37:55 +0000

    Hide empty row when parent directory matches

--- list-git-repos.sh
+++ list-git-repos.sh
@@ -27,6 +27,7 @@ escaped_dir=$(escape_re $directory)
 regex="^${escaped_dir}\/(.*)\/.git$"
 
 find $directory -name .git -type d -prune -maxdepth 2 -print0 | while IFS= read -r -d $'\0' line; do
-    [[ $line =~ $regex ]]
-    echo "${BASH_REMATCH[1]}"
+    if [[ $line =~ $regex ]]; then
+    	echo "${BASH_REMATCH[1]}"
+	fi
 done
