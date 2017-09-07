ec5bb753d25aec775213faa0eedceb45fa892a22
Andrew Bonnington <andrewbonnington@users.noreply.github.com>
2017-09-07 14:11:20 +0100

    Fix indentation

--- list-git-repos.sh
+++ list-git-repos.sh
@@ -12,13 +12,13 @@ else
 			echo "Usage: list-git-repos <directory>"
 			exit
 			;;
-	    *)
+		*)
 	    	if [ -d "$1" ]; then
-				directory=$1
-			else
-				echo "$1 is not a directory"
-				exit
-			fi
+			directory=$1
+		else
+			echo "$1 is not a directory"
+			exit
+		fi
 	    	;;
 	esac
 fi
@@ -27,7 +27,7 @@ escaped_dir=$(escape_re $directory)
 regex="^${escaped_dir}\/(.*)\/.git$"
 
 find $directory -name .git -type d -prune -maxdepth 2 -print0 | while IFS= read -r -d $'\0' line; do
-    if [[ $line =~ $regex ]]; then
-    	echo "${BASH_REMATCH[1]}"
+	if [[ $line =~ $regex ]]; then
+		echo "${BASH_REMATCH[1]}"
 	fi
 done
